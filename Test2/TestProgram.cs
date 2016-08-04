36cc475f9a65cc40bbda8122f029cd85ed3ee9b7
mitenm <miten.morakhia@gmail.com>
2016-08-04 13:51:09 +0530

    initial commit
    commit

---
+++ Test2/TestProgram.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Test2
+{
+    [TestFixture]
+    class ProgramTest
+    {
+        [Test]
+        public void TestInt()
+        {
+            Assert.AreEqual(Program.ReturnFixDigit(), 6);
+        }
+    }
+}
